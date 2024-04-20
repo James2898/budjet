19e22b3daac70e78e2c12aaffce936aed229f174
James Martinez <amajamesmartinez@gmail.com>
2024-04-20 23:39:54 +0800

    Working on Own Firebase

--- config/firebase.js
+++ config/firebase.js
@@ -2,12 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA_xxNnHWD0d3TfORutBUfxQQ1iMQxq-GE",
-  authDomain: "fir-expo-crud.firebaseapp.com",
-  projectId: "fir-expo-crud",
-  storageBucket: "fir-expo-crud.appspot.com",
-  messagingSenderId: "986247513740",
-  appId: "1:986247513740:web:b48e1eb9b105d9dc279da9",
+  apiKey: "AIzaSyDNi52PL7eY98TRRk-GsKa6jJ6yrG7x0tc",
+  authDomain: "badjet.firebaseapp.com",
+  projectId: "badjet",
+  storageBucket: "badjet.appspot.com",
+  messagingSenderId: "1001823520044",
+  appId: "1:1001823520044:web:fe6eb4c94eba6e941ed889",
+  // measurementId: "G-L7287DRQJB"
 };
 
 const app = initializeApp(firebaseConfig);
